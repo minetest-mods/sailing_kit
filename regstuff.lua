d5e2701dd7fa0a3798bb1423804d86bf4cafc9a6
tour <yl_tour@posteo.de>
2026-05-22 12:05:00 +0200

    run stylua

--- regstuff.lua
+++ regstuff.lua
@@ -2,19 +2,19 @@ minetest.register_craftitem("sailing_kit:boat", {
 	description = "Sailboat",
 	inventory_image = "sailboat_inv.png",
 	wield_image = "sailboat_inv.png",
---	wield_scale = {x = 2, y = 2, z = 1},
+	--	wield_scale = {x = 2, y = 2, z = 1},
 	liquids_pointable = true,
---	groups = {flammable = 2},
+	--	groups = {flammable = 2},
 
 	on_place = function(itemstack, placer, pointed_thing)
 		if pointed_thing.type ~= "node" then
 			return itemstack
 		end
-	
+
 		local ppos = pointed_thing.under
 		local node = mobkit.nodeatpos(ppos)
-		if node and node.drawtype=='liquid' then	
-			ppos.y=ppos.y+0.2
+		if node and node.drawtype == "liquid" then
+			ppos.y = ppos.y + 0.2
 			local boat = minetest.add_entity(ppos, "sailing_kit:boat")
 			if boat and placer then
 				boat:set_yaw(placer:get_look_horizontal())
@@ -30,24 +30,23 @@ minetest.register_craftitem("sailing_kit:cloth", {
 	description = "Cloth",
 	inventory_image = "cloth.png",
 	wield_image = "cloth.png",
-	wield_scale = {x = 2, y = 2, z = 1},
-
+	wield_scale = { x = 2, y = 2, z = 1 },
 })
 
 minetest.register_craft({
 	output = "sailing_kit:cloth",
 	recipe = {
-		{"farming:string","farming:string","farming:string"},
-		{"farming:string","farming:string","farming:string"},
-		{"farming:string","farming:string","farming:string"},
+		{ "farming:string", "farming:string", "farming:string" },
+		{ "farming:string", "farming:string", "farming:string" },
+		{ "farming:string", "farming:string", "farming:string" },
 	},
 })
 
 minetest.register_craft({
 	output = "sailing_kit:boat",
 	recipe = {
-		{"farming:string","sailing_kit:cloth","farming:string"},
-		{"group:wood", "sailing_kit:cloth","group:wood"},
-		{"group:wood", "group:wood", "group:wood"},
+		{ "farming:string", "sailing_kit:cloth", "farming:string" },
+		{ "group:wood", "sailing_kit:cloth", "group:wood" },
+		{ "group:wood", "group:wood", "group:wood" },
 	},
 })
